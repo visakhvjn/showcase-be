3f839cf24b1d105ed731afab90526151a2e050c8
Visakh Vijayan <vjnvisakh@gmail.com>
2025-04-24 10:06:35 +0530

    adding cron to clear blogs and caching blogs

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import fs from 'fs';
+import cron from 'node-cron';
 
 const app = express();
 
@@ -7,19 +9,42 @@ app.use(cors());
 
 app.get('/api/medium-feed', async (req, res) => {
 	try {
-		const response = await fetch('https://medium.com/feed/@vjnvisakh');
+		const blogFilePath = './blogs.xml';
 
-		if (!response.ok) {
-			throw new Error(`Failed to fetch Medium feed: ${response.statusText}`);
+		let xmlData = fs.readFileSync(blogFilePath, 'utf8');
+
+		if (!xmlData) {
+			console.log('Fetching Medium feed...');
+			const response = await fetch('https://medium.com/feed/@vjnvisakh');
+
+			if (!response.ok) {
+				throw new Error(`Failed to fetch Medium feed: ${response.statusText}`);
+			}
+
+			xmlData = await response.text();
+
+			fs.writeFileSync(blogFilePath, xmlData, 'utf8');
+		} else {
+			console.log('Using cached Medium feed');
 		}
-
-		const xmlData = await response.text();
 
 		res.set('Content-Type', 'application/xml'); // Set the correct content type
 		res.send(xmlData);
 	} catch (error) {
 		console.error(error);
 		res.status(500).send('Error fetching Medium feed');
+	}
+});
+
+// This cron removes the blogs.xml file every hour
+cron.schedule('0 * * * *', () => {
+	const blogFilePath = './blogs.xml';
+
+	if (fs.existsSync(blogFilePath)) {
+		console.log('Cleared blogs.xml');
+		fs.writeFileSync(blogFilePath, '', 'utf8');
+	} else {
+		console.log('blogs.xml does not exist, skipping deletion');
 	}
 });
 
